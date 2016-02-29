06da93f8de8e563ed44f304694b095668d2df197
A. Darian <git@darian.af>
2016-02-29 13:56:23 +0000

    adding gh-pages

--- README.md
+++
@@ -1,6 +0,0 @@
-Jupyter JS Backbone Widget
-==========================
-
-A Phosphor widget wrapper for Jupyter Backbone widgets.
-
-[API Docs](http://jupyter.github.io/jupyter-js-terminal/)
---
+++ update.sh
@@ -0,0 +1,7 @@
+rm -rf build
+rm *.html
+rm *.css
+cp -r ../jupyter-js-bbwidget/example/build build
+cp -r ../jupyter-js-bbwidget/example/index.html ./
+cp -r ../jupyter-js-bbwidget/example/index.css ./
+cp -r ../jupyter-js-bbwidget/LICENSE ./
